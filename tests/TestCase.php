58d09265063b8916d1dfc23d6935bf0b4acd13a2
Manoz <manoz@outlook.com>
2023-06-26 12:30:00 +0200

    Fix provider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Novius\LaravelNovaNews\Tests;
 
 use Novius\LaravelNovaNews\LaravelNovaNewsServiceProvider;
+use Novius\LaravelPublishable\LaravelPublishableServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
@@ -18,6 +19,7 @@ abstract class TestCase extends Orchestra
     {
         return [
             LaravelNovaNewsServiceProvider::class,
+            LaravelPublishableServiceProvider::class,
         ];
     }
 }
