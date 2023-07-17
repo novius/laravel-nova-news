27c793dedd1af8ec8459c6d04fac34f5b2f1687a
Gilles Felix <felix@novius.fr>
2023-07-17 16:07:24 +0200

    Fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Novius\LaravelNovaNews\Tests;
 
 use Novius\LaravelNovaNews\LaravelNovaNewsServiceProvider;
 use Novius\LaravelPublishable\LaravelPublishableServiceProvider;
+use Novius\LaravelTranslatable\LaravelTranslatableServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
@@ -19,6 +20,7 @@ abstract class TestCase extends Orchestra
     {
         return [
             LaravelNovaNewsServiceProvider::class,
+            LaravelTranslatableServiceProvider::class,
             LaravelPublishableServiceProvider::class,
         ];
     }
