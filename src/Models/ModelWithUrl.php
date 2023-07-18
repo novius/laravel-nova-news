a021ecc77dc5124ef5605ea330eba87af1d16ddc
Gilles Felix <felix@novius.fr>
2023-07-18 16:39:36 +0200

    Fix binding for locale

--- src/Models/ModelWithUrl.php
+++ src/Models/ModelWithUrl.php
@@ -80,14 +80,16 @@ abstract class ModelWithUrl extends Model
 
     public function resolveRouteBinding($value, $field = null)
     {
+        $query = static::where('locale', app()->currentLocale());
+
         if (in_array(Publishable::class, class_uses_recursive($this), true)) {
             $guard = config('laravel-nova-news.guard_preview');
             if (! empty($guard) && Auth::guard($guard)->check()) {
-                return parent::resolveRouteBinding($value, $field);
+                return $this->resolveRouteBindingQuery($query, $value, $field)->first();
             }
 
             if (request()->has('previewToken')) {
-                $query = static::where(function (Builder $query) {
+                $query->where(function (Builder $query) {
                     $query->published()
                         ->orWhere('preview_token', request()->get('previewToken'));
                 });
@@ -96,6 +98,6 @@ abstract class ModelWithUrl extends Model
             }
         }
 
-        return $this->resolveRouteBindingQuery(static::published(), $value, $field)->first();
+        return $this->resolveRouteBindingQuery($query->published(), $value, $field)->first();
     }
 }
